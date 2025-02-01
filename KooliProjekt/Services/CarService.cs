a39217267de332cb350f5c5ca9ff187e3f79ac76
Rasmus Kala <rasmuskala@gmail.com>
2025-02-01 20:39:43 +0200

    Service for CarsController done

---
+++ KooliProjekt/Services/CarService.cs
@@ -0,0 +1,51 @@
+﻿using KooliProjekt.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace KooliProjekt.Services
+{
+    public class CarService : ICarService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CarService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task <List<Car>> AllCars()
+        {
+            return await _context.Cars
+                .Include(car => car.Operations)
+                .ToListAsync();
+        }
+        
+        public async Task<Car> Get(int id)
+        {
+            return await _context.Cars
+                .Include(car => car.Operations)
+                .Where(car => car.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task Save(Car car)
+        {
+            if (car.Id == 0)
+            {
+                _context.Cars.Add(car);
+            }
+            else
+            {
+                _context.Cars.Update(car);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            await _context.Cars
+                .Where(car => car.Id == id)
+                .ExecuteDeleteAsync();
+        }
+    }
+}
---
+++ KooliProjekt/Services/ICarService.cs
@@ -0,0 +1,13 @@
+﻿using KooliProjekt.Data;
+
+namespace KooliProjekt.Services
+{
+    public interface ICarService
+    {
+        Task<List<Car>> AllCars();
+        Task<Car> Get(int id);
+        Task Save(Car car);
+        Task Delete(int id);
+
+    }
+}
