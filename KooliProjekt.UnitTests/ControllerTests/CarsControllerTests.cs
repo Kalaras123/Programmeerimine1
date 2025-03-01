acf6b0916c9047e25fea7b811098ebd8b84b980c
Rasmus Kala <rasmuskala@gmail.com>
2025-03-01 22:54:27 +0200

    HomeControllerTests, CarsControllerTests for Index

---
+++ KooliProjekt.UnitTests/ControllerTests/CarsControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using KooliProjekt.Services;
+using KooliProjekt.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using KooliProjekt.Data;
+
+namespace KooliProjekt.UnitTests.ControllerTests
+{
+    public class CarsControllerTests
+    {
+        private readonly Mock<ICarService> _carServiceMock;
+        private readonly CarsController _controller;
+
+        public CarsControllerTests()
+        {
+            _carServiceMock = new Mock<ICarService>();
+            _controller = new CarsController(_carServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task Index_should_return_view_and_data()
+        {
+            //Arrange
+            var data = new List<Car>
+            {
+                new Car { Id = 1, Mark = "Audi", Model = "A4", RegistrationNumber = "123ABC" },
+                new Car { Id = 2, Mark = "BMW", Model = "X5", RegistrationNumber = "456DEF" }
+            };
+            _carServiceMock.Setup(x => x.AllCars()).ReturnsAsync(data);
+
+            //Act
+            var result = await _controller.Index() as ViewResult;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Index"
+                );
+            Assert.Equal(data, result.Model);
+        }
+    }
+}
--- KooliProjekt.UnitTests/ControllerTests/HomeControllerTests.cs
+++ KooliProjekt.UnitTests/ControllerTests/HomeControllerTests.cs
@@ -1,5 +1,7 @@
 ﻿using KooliProjekt.Controllers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using Xunit;
 
 namespace KooliProjekt.UnitTests.ControllerTests
@@ -7,18 +9,74 @@ namespace KooliProjekt.UnitTests.ControllerTests
     public class HomeControllerTests
     {
         [Fact]
-        public void Index_should_return_index_view()
+        public void Index_should_return_correct_view()
         {
-            // Arrange
+            //Arrange
             var controller = new HomeController();
 
-            // Act
+            //Act
             var result = controller.Index() as ViewResult;
 
-            // Assert
+            //Assert
             Assert.NotNull(result);
-            Assert.True(result.ViewName == "Index" ||
-                        string.IsNullOrEmpty(result.ViewName));
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) || 
+                result.ViewName == "Index"
+                );
+        }
+
+        [Fact]
+        public void Privacy_should_return_correct_view()
+        {
+            //Arrange
+            var controller = new HomeController();
+
+            //Act
+            var result = controller.Privacy() as ViewResult;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Privacy"
+                );
+        }
+
+        [Fact]
+        public void Error_should_return_correct_view_with_httpcontext()
+        {
+            //Arrange
+            var controller = new HomeController();
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            //Act
+            var result = controller.Error() as ViewResult;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Error"
+                );
+        }
+
+        [Fact]
+        public void Error_should_return_correct_view_with_current_activity()
+        {
+            //Arrange
+            var controller = new HomeController();
+            Activity.Current = new Activity("activity").Start();
+
+            //Act
+            var result = controller.Error() as ViewResult;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Error"
+                );
         }
     }
 }
